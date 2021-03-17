a1ff1e27157b3c8051d93d374003ce7c75ef12f1
Diomezio Almeida <diomezio_almeida@hotmail.com>
2021-03-16 22:01:57 -0300

    aula java script index

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+function clicou(){
+    alert("Obrigado por clicar!");
+
+}
+
+function direcionar(){
+    window.open("https://www.google.com.br/");
+}
+
+var nome = "Diomezio Almeida";
+alert (nome);
